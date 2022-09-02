6646373556c7ff359237402d908152ee28f6cdb4
Misael  M <misaelmdev@gmail.com>
2022-09-02 17:19:57 +1000

    added basic styles to base app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-       <h1>React dumb app for testing github action</h1>
-       <button>This another dumb buttom</button>
-      </header>
-    </div>
-  );
+		<div className='App'>
+			<header className='App-header'>
+				<h1 class='text-4xl mb-10'>React dumb app for testing github action</h1>
+				<button class='bg-black delay-150 ease-in-out hover:opacity-70 p-5 rounded-xl text-xl text-yellow-200 transition-opacity'>
+					This another dumb buttom
+				</button>
+			</header>
+		</div>
+	);
 }
 
 export default App;
