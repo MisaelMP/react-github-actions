c6cf6b50661f8633f8b6419df137f90b3b816d24
Misael  M <misaelmdev@gmail.com>
2022-09-03 22:38:22 +1000

    a bit more of structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
-
 function App() {
   return (
-		<div className='App'>
-			<header className='App-header'>
-				<h1 class='text-4xl mb-10'>React dumb app for testing github action</h1>
-				<button class='bg-black delay-150 ease-in-out hover:opacity-70 p-5 rounded-xl text-xl text-yellow-200 transition-opacity'>
+		<div>
+			<header>
+				<h1 className='text-4xl mb-10'>React dumb app for testing github action</h1>
+				<button className='bg-black delay-150 ease-in-out hover:opacity-70 p-5 rounded-xl text-xl text-yellow-200 transition-opacity'>
 					This another dumb buttom
 				</button>
 			</header>
