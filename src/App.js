22449e773f4e8e898fce23e167e677a3834ccb95
Misael  M <misaelmdev@gmail.com>
2022-09-05 23:27:08 +1000

    added tailwindcss to styled components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
+import Button from './styles/components/button';
+
 function App() {
   return (
 		<div>
 			<header>
 				<h1 className='text-4xl mb-10'>React dumb app for testing github action</h1>
-				<button className='bg-black delay-150 ease-in-out hover:opacity-70 p-5 rounded-xl text-xl text-yellow-200 transition-opacity'>
+				<Button>
 					This another dumb buttom
-				</button>
+				</Button>
 			</header>
 		</div>
 	);
---
+++ src/styles/components/button.js
@@ -0,0 +1,5 @@
+import tw from 'twin.macro';
+
+const Button = tw.button`bg-black delay-150 ease-in-out hover:opacity-70 p-5 rounded-xl text-xl text-yellow-200 transition-opacity`;
+
+export default Button;
